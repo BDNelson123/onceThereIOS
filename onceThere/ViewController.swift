6fcb45e39d91acf2ad38392b439935d30c2ac2bd
Benjamin Nelson <benjaminnelson@Benjamins-MacBook-Pro.local>
2015-11-20 14:22:57 -0600

    first parts of oncethere ipad app

---
+++ onceThere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  onceThere
+//
+//  Created by Benjamin Nelson on 11/20/15.
+//  Copyright © 2015 Benjamin Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
